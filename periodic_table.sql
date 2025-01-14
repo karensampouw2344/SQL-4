275807f3b0d6475a659098403067766eb2445e67
Karen <k4r3nsampouw@gmail.com>
2025-01-14 19:25:22 +0700

    Add files via upload

---
+++ periodic_table.sql
@@ -0,0 +1,112 @@
+CREATE TABLE elements (
+    atomic_number INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    atomic_weight REAL,
+    group_number INTEGER,
+    period_number INTEGER
+);
+-- Hydrogen to Helium
+INSERT INTO elements (atomic_number, name, symbol, atomic_weight, group_number, period_number)
+VALUES (1, 'Hydrogen', 'H', 1.008, 1, 1),
+       (2, 'Helium', 'He', 4.0026, 18, 1);
+
+-- Lithium to Neon
+INSERT INTO elements (atomic_number, name, symbol, atomic_weight, group_number, period_number)
+VALUES (3, 'Lithium', 'Li', 6.94, 1, 2),
+       (4, 'Beryllium', 'Be', 9.0122, 2, 2),
+       (5, 'Boron', 'B', 10.81, 13, 2),
+       (6, 'Carbon', 'C', 12.011, 14, 2),
+       (7, 'Nitrogen', 'N', 14.007, 15, 2),
+       (8, 'Oxygen', 'O', 15.999, 16, 2),
+       (9, 'Fluorine', 'F', 18.998, 17, 2),
+       (10, 'Neon', 'Ne', 20.180, 18, 2);
+
+-- Sodium to Argon
+INSERT INTO elements (atomic_number, name, symbol, atomic_weight, group_number, period_number)
+VALUES (11, 'Sodium', 'Na', 22.990, 1, 3),
+       (12, 'Magnesium', 'Mg', 24.305, 2, 3),
+       (13, 'Aluminum', 'Al', 26.982, 13, 3),
+       (14, 'Silicon', 'Si', 28.085, 14, 3),
+       (15, 'Phosphorus', 'P', 30.974, 15, 3),
+       (16, 'Sulfur', 'S', 32.06, 16, 3),
+       (17, 'Chlorine', 'Cl', 35.45, 17, 3),
+       (18, 'Argon', 'Ar', 39.948, 18, 3);
+
+-- Potassium to Krypton
+INSERT INTO elements (atomic_number, name, symbol, atomic_weight, group_number, period_number)
+VALUES (19, 'Potassium', 'K', 39.098, 1, 4),
+       (20, 'Calcium', 'Ca', 40.078, 2, 4),
+       (21, 'Scandium', 'Sc', 44.956, 3, 4),
+       (22, 'Titanium', 'Ti', 47.867, 4, 4),
+       (23, 'Vanadium', 'V', 50.942, 5, 4),
+       (24, 'Chromium', 'Cr', 52.0, 6, 4),
+       (25, 'Manganese', 'Mn', 54.938, 7, 4),
+       (26, 'Iron', 'Fe', 55.845, 8, 4),
+       (27, 'Cobalt', 'Co', 58.933, 9, 4),
+       (28, 'Nickel', 'Ni', 58.693, 10, 4),
+       (29, 'Copper', 'Cu', 63.546, 11, 4),
+       (30, 'Zinc', 'Zn', 65.38, 12, 4),
+       (31, 'Gallium', 'Ga', 69.723, 13, 4),
+       (32, 'Germanium', 'Ge', 72.63, 14, 4),
+       (33, 'Arsenic', 'As', 74.922, 15, 4),
+       (34, 'Selenium', 'Se', 78.971, 16, 4),
+       (35, 'Bromine', 'Br', 79.904, 17, 4),
+       (36, 'Krypton', 'Kr', 83.798, 18, 4);
+
+-- Rubidium to Xenon
+INSERT INTO elements (atomic_number, name, symbol, atomic_weight, group_number, period_number)
+VALUES (37, 'Rubidium', 'Rb', 85.468, 1, 5),
+       (38, 'Strontium', 'Sr', 87.62, 2, 5),
+       (39, 'Yttrium', 'Y', 88.906, 3, 5),
+       (40, 'Zirconium', 'Zr', 91.224, 4, 5),
+       (41, 'Niobium', 'Nb', 92.906, 5, 5),
+       (42, 'Molybdenum', 'Mo', 95.95, 6, 5),
+       (43, 'Technetium', 'Tc', 98, 7, 5),
+       (44, 'Ruthenium', 'Ru', 101.07, 8, 5),
+       (45, 'Rhodium', 'Rh', 102.91, 9, 5),
+       (46, 'Palladium', 'Pd', 106.42, 10, 5),
+       (47, 'Silver', 'Ag', 107.87, 11, 5),
+       (48, 'Cadmium', 'Cd', 112.41, 12, 5),
+       (49, 'Indium', 'In', 114.82, 13, 5),
+       (50, 'Tin', 'Sn', 118.71, 14, 5),
+       (51, 'Antimony', 'Sb', 121.76, 15, 5),
+       (52, 'Tellurium', 'Te', 127.6, 16, 5),
+       (53, 'Iodine', 'I', 126.90, 17, 5),
+       (54, 'Xenon', 'Xe', 131.29, 18, 5);
+
+-- Cesium to Radon
+INSERT INTO elements (atomic_number, name, symbol, atomic_weight, group_number, period_number)
+VALUES (55, 'Cesium', 'Cs', 132.91, 1, 6),
+       (56, 'Barium', 'Ba', 137.33, 2, 6),
+       (57, 'Lanthanum', 'La', 138.91, 3, 6),
+       (58, 'Cerium', 'Ce', 140.12, 4, 6),
+       (59, 'Praseodymium', 'Pr', 140.91, 5, 6),
+       (60, 'Neodymium', 'Nd', 144.24, 6, 6),
+       (61, 'Promethium', 'Pm', 145, 7, 6),
+       (62, 'Samarium', 'Sm', 150.36, 8, 6),
+       (63, 'Europium', 'Eu', 151.98, 9, 6),
+       (64, 'Gadolinium', 'Gd', 157.25, 10, 6),
+       (65, 'Terbium', 'Tb', 158.93, 11, 6),
+       (66, 'Dysprosium', 'Dy', 162.50, 12, 6),
+       (67, 'Holmium', 'Ho', 164.93, 13, 6),
+       (68, 'Erbium', 'Er', 167.26, 14, 6),
+       (69, 'Thulium', 'Tm', 168.93, 15, 6),
+       (70, 'Ytterbium', 'Yb', 173.04, 16, 6),
+       (71, 'Lutetium', 'Lu', 175.00, 17, 6),
+       (72, 'Hafnium', 'Hf', 178.49, 4, 7),
+       (73, 'Tantalum', 'Ta', 180.95, 5, 7),
+       (74, 'Tungsten', 'W', 183.84, 6, 7),
+       (75, 'Rhenium', 'Re', 186.21, 7, 7),
+       (76, 'Osmium', 'Os', 190.23, 8, 7),
+       (77, 'Iridium', 'Ir', 192.22, 9, 7),
+       (78, 'Platinum', 'Pt', 195.08, 10, 7),
+       (79, 'Gold', 'Au', 196.97, 11, 7),
+       (80, 'Mercury', 'Hg', 200.59, 12, 7),
+       (81, 'Thallium', 'Tl', 204.38, 13, 7),
+       (82, 'Lead', 'Pb', 207.2, 14, 7),
+       (83, 'Bismuth', 'Bi', 208.98, 15, 7),
+       (84, 'Polonium', 'Po', 209, 16, 7),
+       (85, 'Astatine', 'At', 210, 17, 7),
+       (86, 'Radon', 'Rn', 222, 18, 7);
+
